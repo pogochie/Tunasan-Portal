3c5940daf96d206a9528d4431a841b89a11669a6
pogochie <pogo2346@gmail.com>
2026-01-02 12:48:00 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,6 @@ const path = require('path');
 const fs = require('fs');
 
 const uploadsDir = path.join(__dirname, 'uploads');
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '../uploads'));
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-const imagePaths = req.files.map(file => `/uploads/${file.filename}`);
 
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
