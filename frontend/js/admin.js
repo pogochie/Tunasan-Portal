51e6afdcd7edfaa6fbf3a616515cd2f8907c32c4
pogochie <pogo2346@gmail.com>
2026-01-02 11:54:33 +0800

    test

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -13,12 +13,19 @@ async function loadIncidents() {
         <td>${i.incidentType}</td>
         <td>${i.status}</td>
         <td>
-          <button onclick="reviewReport('${i._id}')">Review Report</button>
+          <button class="review-btn" data-id="${i._id}">Review Report</button>
         </td>
       </tr>
     `;
   });
 }
+
+tableBody.addEventListener("click", (e) => {
+  if (e.target.classList.contains("review-btn")) {
+    const id = e.target.getAttribute("data-id");
+    window.location.href = `review.html?id=${id}`;
+  }
+});
 
 function reviewReport(id) {
   window.location.href = `review.html?id=${id}`;
